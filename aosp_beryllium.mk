71136b2f3978b39c52a5e028a8cc90a9869e1bba
Naruto-242 <144617151+Naruto-242@users.noreply.github.com>
2023-10-02 14:50:39 +0530

    beryllium: Set Boot animation to 1080p

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -6,7 +6,8 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.

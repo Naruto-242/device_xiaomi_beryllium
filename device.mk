e75c3f14113c07458ddc2c25eacd0cf0ebd8e184
Nikhil <Naruto242@proton.me>
2023-10-05 14:12:35 +0530

    Revert: Beryllium FU overlay

--- device.mk
+++ device.mk
@@ -25,8 +25,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    NoCutoutOverlay \
-     BerylliumFaceUnlockServiceOverlay
+    NoCutoutOverlay 
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
